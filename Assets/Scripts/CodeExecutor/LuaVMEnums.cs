22cd70653d7d3f79d837b5b71e7ce7743f61ec2c
ChensenCHX <2087826155@qq.com>
2026-01-12 12:26:09 +0800

    basic enums for code executor

---
+++ Assets/Scripts/CodeExecutor/LuaVMEnums.cs
@@ -0,0 +1,18 @@
+namespace CodeExecutor
+{
+    public enum RunningState
+    {
+        Ready,          // ready to run
+        Waiting,        // waiting for resume main thread
+        Finished,       // execution finished
+        Faulted,        // lua vm occured a error | vm internal error
+        Terminated,     // terminated (probably by user) and should be discarded
+    }
+
+    public enum InfoHookMode
+    {
+        Normal,         // normal mode
+        NextLine,       // single line step-in
+        LineBreakPoint, // line breakpoint mode
+    }
+}
