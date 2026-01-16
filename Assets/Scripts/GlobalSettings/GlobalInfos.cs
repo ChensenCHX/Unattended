f85a0374ef69610af86d37e1f9a6b15771a5bee5
ChensenCHX <2087826155@qq.com>
2026-01-17 00:02:07 +0800

    add global settings & infos

---
+++ Assets/Scripts/GlobalSettings/GlobalInfos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Utils;
+
+namespace GlobalSettings
+{
+    public class GlobalInfos : Singleton<GlobalInfos>
+    {
+        public const int MaxWorkspaceEdgeLength = 32;
+        public int WorkspaceEdgeLength = 1;
+    }
+}
