bbc789c4a2c1664e63e5de8860e172efd649aa41
ChensenCHX <2087826155@qq.com>
2026-01-17 00:04:18 +0800

    add items

---
+++ Assets/Scripts/Items/ItemType.cs
@@ -0,0 +1,35 @@
+﻿namespace Items
+{
+    public enum ItemType
+    {
+        // 实际上是现在有的所有模型
+        # region from Miner
+        OreGold,
+        OreSilver,
+        OreCopper,
+        OreZinc,
+        OreIron,
+        OreNickel,
+        OreCoal,
+        # endregion
+        
+        # region from Refiner
+        RefinedGold,
+        RefinedSilver,
+        RefinedCopper,
+        RefinedZinc,
+        RefinedIron,
+        RefinedNickel,
+        RefinedCoal,
+        # endregion
+        
+        # region from Smelter
+        IngotGold,
+        IngotSilver,
+        IngotCopper,
+        IngotZinc,
+        IngotIron,
+        IngotNickel, 
+        # endregion
+    }
+}
