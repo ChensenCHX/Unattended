a8c758b340e712883a4cf7c48b8a075e5d279177
ChensenCHX <2087826155@qq.com>
2026-01-20 00:57:50 +0800

    test prefabs & scene

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,46 @@
+﻿using CodeExecutor;
+using GlobalSettings;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private LuaVM luaVM;
+    void Start()
+    {
+        luaVM = new LuaVM(new LuaVMConfigurer(8,
+            vm => {
+                LuaVMAdaptorLib.AtomicCAS(vm); LuaVMAdaptorLib.CheckThread(vm);
+                LuaVMAdaptorLib.GetCurrentThreadID(vm); LuaVMAdaptorLib.HangupCurrentThread(vm);
+                LuaVMAdaptorLib.NewThread(vm); LuaVMAdaptorLib.Move(vm);
+                LuaVMAdaptorLib.CanHarvest(vm); LuaVMAdaptorLib.Harvest(vm);
+                vm.GetLuaVM().Options.DebugPrint = Debug.Log;
+            }, LuaVMAdaptorLib.DestroyAllBots, LuaVMAdaptorLib.CheckCurrentBotIsBusy), 
+            "TestScript", @"
+function f()
+    print('Thread id:', get_current_thread())
+    move(2) move(2) move(2) move(2) move(2) move(2) move(2) move(2)
+    move(2) move(2) move(2) move(2) move(2) move(2) move(2) move(2)
+    while true do end
+end
+
+print('Thread id:', get_current_thread())
+print('New thread:', new_thread(f))
+print(can_harvest())
+print(harvest())
+harvest() harvest() harvest() harvest() harvest()
+while true do end
+            ");
+    }
+
+    void Update()
+    {
+        if (luaVM.CouldResume())
+            luaVM.ResumeUntilLimit(LuaVMConfigurer.MaxInstructionPerResume);
+        else
+        {
+            Debug.Log(luaVM.State);
+            if (luaVM.State == RunningState.Faulted) Debug.Log(luaVM.ExceptionWhat);
+            Destroy(gameObject);
+        }
+    }
+}
