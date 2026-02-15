1ff6de68c7297103342d81452975072868d0fd8b
ChensenCHX <2087826155@qq.com>
2026-02-15 21:54:45 +0800

    add dragger for adjust window size manually

---
+++ Assets/Scripts/EditorUIAdaptor/Behaviours/DraggerAdjustor.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
+
+namespace EditorUIAdaptor.Behaviours
+{
+    public enum DraggerSide
+    {
+        Right,
+        Bottom,
+        Corner,
+    }
+    
+    public class DraggerAdjustor : MonoBehaviour, IDragHandler, IEndDragHandler, IPointerEnterHandler, IPointerExitHandler
+    {
+        [SerializeField] private WindowAdjustor windowAdjustor;
+        [SerializeField] private DraggerSide side;
+
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            var newSize = side switch
+            {
+                DraggerSide.Right   => new Vector2(eventData.delta.x, 0) + windowAdjustor.mainWindow.sizeDelta,
+                DraggerSide.Bottom  => new Vector2(0, -eventData.delta.y) + windowAdjustor.mainWindow.sizeDelta,
+                DraggerSide.Corner  => new Vector2(eventData.delta.x, -eventData.delta.y) + windowAdjustor.mainWindow.sizeDelta,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+            windowAdjustor.TryResizeWindow(newSize.x, newSize.y);
+        }
+        public void OnEndDrag(PointerEventData eventData) => EventSystem.current.SetSelectedGameObject(UIBGMouseListener.Instance.gameObject);
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            // TODO:: change cursor type here
+        }
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            // TODO:: change cursor type here
+        }
+    }
+}
