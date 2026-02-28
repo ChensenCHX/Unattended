003355f5416696b237ccadb9796a3bf4c1345b3c
ChensenCHX <2087826155@qq.com>
2026-02-28 15:58:39 +0800

    basic dragger & close button for info window

---
+++ Assets/Scripts/UI/InGameUI/InfoWindow/CloseInfoWindow.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace UI.InGameUI.InfoWindow
+{
+    public class CloseInfoWindow : MonoBehaviour
+    {
+        [SerializeField] private GameObject windowObject;
+        
+        public void OnClickThis() => Destroy(windowObject);
+    }
+}
---
+++ Assets/Scripts/UI/InGameUI/InfoWindow/DraggerAdujstor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using EditorUIAdaptor.Behaviours;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UI.InGameUI.InfoWindow
+{
+    public enum DraggerSide
+    {
+        Right,
+        Bottom,
+        Corner,
+    }
+    
+    public class DraggerAdjustor : MonoBehaviour, IDragHandler, IEndDragHandler
+    {
+        [SerializeField] private RectTransform windowRectTransform;
+        [SerializeField] private DraggerSide side;
+        
+        public void OnDrag(PointerEventData eventData)
+        {
+            windowRectTransform.sizeDelta = side switch
+            {
+                DraggerSide.Right   => new Vector2(eventData.delta.x, 0) + windowRectTransform.sizeDelta,
+                DraggerSide.Bottom  => new Vector2(0, -eventData.delta.y) + windowRectTransform.sizeDelta,
+                DraggerSide.Corner  => new Vector2(eventData.delta.x, -eventData.delta.y) + windowRectTransform.sizeDelta,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+        public void OnEndDrag(PointerEventData eventData) => EventSystem.current.SetSelectedGameObject(UIBGMouseListener.Instance.gameObject);
+    }
+}
+
