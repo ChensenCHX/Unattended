5f3e60c8f122a2931c00cd199cc1177da95125ed
ChensenCHX <2087826155@qq.com>
2026-03-04 22:47:49 +0800

    detect esc key

--- Assets/Scripts/UI/MenuUI/MenuController.cs
+++ Assets/Scripts/UI/MenuUI/MenuController.cs
@@ -1,4 +1,6 @@
+using UnityEditor.EditorTools;
 using UnityEngine;
+using UnityEngine.UI;
 using Utils;
 
 namespace UI.MenuUI
@@ -8,14 +10,27 @@ namespace UI.MenuUI
         [SerializeField] private GameObject inGameUIOverlayObject;
         [SerializeField] private GameObject worldUICanvasObject;
         [SerializeField] private GameObject screenUICanvasObject;
+        [SerializeField] private GameObject menuUIRootObject;
         
+        [Header("Inner State References")]
+        [SerializeField] private Toggle switchUIButton;
+        
+        private bool isActive;
         private void SetActive(bool active)
         {
             inGameUIOverlayObject.SetActive(active);
             worldUICanvasObject.SetActive(active);
             screenUICanvasObject.SetActive(active);
-            gameObject.SetActive(!active);
+            menuUIRootObject.SetActive(!active);
+            isActive = active;
         } 
+        
         private void Start() => SetActive(false);
+        private void Update()
+        {
+            if (!Input.GetKeyDown(KeyCode.Escape)) return;
+            if (switchUIButton.isOn) { switchUIButton.isOn = false; return; }
+            SetActive(!isActive);
+        }
     }
 }
