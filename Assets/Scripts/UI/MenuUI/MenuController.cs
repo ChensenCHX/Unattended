c5a4896da6b42dd5cef4fa1c1eb4989f78074793
ChensenCHX <2087826155@qq.com>
2026-04-13 22:13:38 +0800

    add menu stack && settings page(part of)

--- Assets/Scripts/UI/MenuUI/MenuController.cs
+++ Assets/Scripts/UI/MenuUI/MenuController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Save;
 using UnityEditor.EditorTools;
 using UnityEngine;
@@ -12,6 +13,9 @@ namespace UI.MenuUI
         [SerializeField] private GameObject worldUICanvasObject;
         [SerializeField] private GameObject screenUICanvasObject;
         [SerializeField] private GameObject menuUIRootObject;
+        [SerializeField] private List<GameObject> menuPages;
+        
+        private HistoryStack<GameObject> historyStack;
         
         [Header("Inner State References")]
         [SerializeField] private Toggle switchUIButton;
@@ -27,7 +31,26 @@ namespace UI.MenuUI
             if (active && isNotFirst) SaveManager.SaveAll();
         } 
         public void SwitchState() => SetActive(!isActive);
+
+        public void PushPage(int pageIndex)
+        {
+            if  (pageIndex < 0 || pageIndex >= menuPages.Count) throw new System.IndexOutOfRangeException();
+            historyStack.Peek().SetActive(false);
+            historyStack.Push(menuPages[pageIndex]);
+            historyStack.Peek().SetActive(true);
+        }
+        public int PopPage()
+        {
+            var obj = historyStack.Pop();
+            obj.SetActive(false);
+            historyStack.Peek().SetActive(true);
+            return menuPages.IndexOf(obj);
+        }
         
+        protected override void OnAwake()
+        {
+            historyStack = new HistoryStack<GameObject>(menuPages[0]);
+        }
         private void Start()
         {
             SetActive(true, false);
@@ -36,6 +59,8 @@ namespace UI.MenuUI
         {
             if (!Input.GetKeyDown(KeyCode.Escape)) return;
             if (switchUIButton.isOn) { switchUIButton.isOn = false; return; }
+            if (historyStack.Count != 0) { PopPage(); return; }
+
             SetActive(!isActive);
         }
     }
